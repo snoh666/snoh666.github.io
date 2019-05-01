e8b1d705cefa396c66bd3ab4d10e5bb08c92d6e3
snoh666 <snochopl@gmail.com>
2019-05-01 22:52:12 +0200

    Added working github API fetch with all handling

--- js/index.js
+++ js/index.js
@@ -1,10 +1,55 @@
-// fetch('https://api.github.com/users/snoh666')
-//   .then(data => data.json())
-//   .then(data => {
+fetch('https://api.github.com/users/snoh666/repos')
+  .then(data => data.json())
+  .then(data => {
 
-//     const root;
+    const webContentBox = document.getElementsByClassName('web-content')[0];
+    webContentBox.innerHTML = '';
+    data.forEach(element => {
+      /*
+        Wrap everything in 'a' link or add click listener
+        element =
+        name = name of repo
+        description = description of repo
+        homepage = link of homepage repo
+        language = language of repo
+        HTML, JAVASCRIPT, CSS
+        #e34c26, #f1e05a, #563d7c
+      */
+      const linkFunction = function (link) {
+        console.log(link);
+      };
+      const repoItems = [document.createElement('div'), document.createElement('div'), document.createElement('div')];
+      repoItems[0].setAttribute('class', 'repo-name');
+      let repoName = element.name.replace('-', ' ').replace('-', ' ').replace('_', ' ');
+      console.log(repoName);
+      repoItems[0].appendChild(document.createTextNode(repoName));
+      repoItems[1].setAttribute('class', 'repo-desc');
+      repoItems[1].appendChild(document.createTextNode(element.description));
+      if(element.language == 'JavaScript'){
+        repoItems[2].setAttribute('class', 'repo-lang javascript');
+      } else if(element.language == 'CSS') {
+        repoItems[2].setAttribute('class', 'repo-lang css');
+      } else if(element.language == 'HTML') {
+        repoItems[2].setAttribute('class', 'repo-lang html');
+      } else {
+        repoItems[2].setAttribute('class', 'repo-lang ');
+      }
+      repoItems[2].appendChild(document.createTextNode(element.language));
+      const mainReposElem = document.createElement('div');
+      repoItems.forEach(element => {
+        mainReposElem.appendChild(element);
+      });
+      mainReposElem.setAttribute('class', `git-repo ${element.name}`);
+      webContentBox.appendChild(mainReposElem);
+      if(element.homepage === '') {
+        console.warn(`${element.name}: homepage isnt avaible`);
+      } else {
+        const repoElement = document.getElementsByClassName(element.name)[0];
+        repoElement.classList.add('homepage-active');
+        repoElement.addEventListener('click', () => {
+          window.location = element.homepage;
+        });
+      }
+    });
 
-//     root.innerHTML = data.login;
-//     root.innerHTML += `<img src=${data.avatar_url}/>`;
-
-//   });
+  });
