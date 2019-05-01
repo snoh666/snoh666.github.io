0a9b36e95685280ae05b18534122684e435b9b64
snoh666 <snochopl@gmail.com>
2019-05-01 16:07:58 +0200

    Added commented out github api connection

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// fetch('https://api.github.com/users/snoh666')
+//   .then(data => data.json())
+//   .then(data => {
+
+//     const root;
+
+//     root.innerHTML = data.login;
+//     root.innerHTML += `<img src=${data.avatar_url}/>`;
+
+//   });
